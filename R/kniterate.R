b62e06075581c6aea3fac561f55cdfded5e7db5a
Eric Kernfeld <eric.kern13@gmail.com>
2017-08-19 12:48:01 -0400

    Wrote package. (This was a runt!)

---
+++ R/kniterate.R
@@ -0,0 +1,12 @@
+#' Given the root of a package, knit .Rmd files into R code.
+#'
+#' @export
+#'
+kniterate = function( package_root = getwd() ){
+  package_code_rmd = list.files( file.path(package_root, "R") )
+  package_code_rmd = package_code_rmd[]
+  package_code_r = gsub( package_code_r, pattern = ".Rmd", replacement = ".R", ignore.case = T, fixed = T)
+  for(i in seq_along(package_code_r)){
+    knitr::purl(file.path( "R", package_code_rmd[[i]] ), output = file.path( "R", package_code_r  [[i]] ) )
+  }
+}
